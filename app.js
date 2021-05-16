e3e9c4298d393f374f35fe9af4f957459a91e6c5
R0n1nK <51276604+R0n1nK@users.noreply.github.com>
2021-05-16 15:29:29 +0200

    commit

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let outputDivAddress = document.getElementById('outputDivAddress');
-
+let outputDivTx = document.getElementById('outputDivTx');
+let outputDivBlock = document.getElementById('outputDivBlock');
 
 $('#cardList a').on('click', function (e) {
     $(this).tab('show')
     outputDivAddress.style.display = 'none'
 })
-
-/*0*/
 
 /*Buttons*/
 let addressBtn = document.getElementById('addrBtn');
@@ -36,7 +35,9 @@ addressBtn.addEventListener("click", function () {
             document.getElementById("total_received_paragraph").innerHTML = 'Total Received: ' + received + ' BTC';
             document.getElementById("total_sent_paragraph").innerHTML = 'Total Sent: ' + sent + ' BTC';
             document.getElementById("balance_paragraph").innerHTML = 'Balance: ' + balance + ' BTC';
+            
             outputDivTx.style.display = 'none';
+            outputDivBlock.style.display = 'none'
             outputDivAddress.style.display = 'block';
         });
     }
@@ -57,7 +58,7 @@ txBtn.addEventListener("click", function () {
             let prev_addresses = data.data.inputs[0];
             let addresses = data.data.outputs[0];
 
-            let date = new Date(block_time*1000);
+            let date = new Date(block_time * 1000);
 
             if (block_height == -1) {
                 document.getElementById("status").innerHTML = 'Status: Unconfirmed';
@@ -78,7 +79,50 @@ txBtn.addEventListener("click", function () {
             document.getElementById("inputs_value").innerHTML = 'Total Input: ' + inputs_value + ' BTC';
             document.getElementById("outputs_value").innerHTML = 'Total Output: ' + outputs_value + ' BTC'
 
+            outputDivAddress.style.display = 'none';
+            outputDivBlock.style.display = 'none'
             outputDivTx.style.display = 'block';
+            
+        });
+    }
+})
+
+blockBtn.addEventListener("click", function () {
+    if (blockInput.value.length == 0) {
+    }
+    else {
+        $.getJSON('https://chain.api.btc.com/v3/block/' + blockInput.value, function (data) {
+            let timestamp = data.data.timestamp;
+            let hash = data.data.hash;
+            let height = data.data.height;
+            let size = data.data.size;
+            let difficulty = data.data.difficulty / 100000000;
+            let pool_difficulty = data.data.pool_difficulty / 100000000;
+            let tx_count = data.data.tx_count;
+            let reward_block = data.data.reward_block / 100000000;
+            let reward_fees = data.data.reward_fees / 100000000;
+            let confirmations = data.data.confirmations;
+            let pool_name = data.data.extras.pool_name;
+            let pool_link = data.data.extras.pool_link;
+
+            let date = new Date(timestamp * 1000);
+
+            document.getElementById("hashBlock").innerHTML = 'Hash: ' + hash;
+            document.getElementById("height").innerHTML = 'Height: ' + height
+            document.getElementById("timestamp").innerHTML = 'Timestamp: ' + date
+            document.getElementById("size").innerHTML = 'Size: ' + size + ' Bytes'
+            document.getElementById("difficulty").innerHTML = 'Difficulty: ' + difficulty
+            document.getElementById("pool_difficulty").innerHTML = 'Pool difficulty: ' + pool_difficulty
+            document.getElementById("tx_count").innerHTML = 'Transactions: ' + tx_count
+            document.getElementById("reward_block").innerHTML = 'Reward block: ' + reward_block
+            document.getElementById("reward_fees").innerHTML = 'Reward fees: ' + reward_fees
+            document.getElementById("confirmations_block").innerHTML = 'Confirmations: ' + confirmations
+            document.getElementById("pool_name").innerHTML = 'Miner: ' + pool_name
+            document.getElementById("pool_link").innerHTML = 'Pool link: <a href=' + pool_link + '>' + pool_link + '</a>';
+
+            outputDivTx.style.display = 'none';
+            outputDivAddress.style.display = 'none';
+            outputDivBlock.style.display = 'block'
         });
     }
 })
